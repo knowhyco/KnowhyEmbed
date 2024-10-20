ea3f62cfa57f17b0fbe3b59159e3eb6ef561e0f5
knowhyco <info@knowhy.co>
2024-10-20 13:06:00 +0000

    KnowhyEmbedUpdate

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,14 +51,14 @@ export const Badge = (props: Props) => {
           {props.footer?.text ?? 'Geliştirici by'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://knowhy.com'}
+            href={props.footer?.companyLink ?? 'https://knowhy.co'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Knowhy'}</span>
           </a>
         </span>
       </Show>
